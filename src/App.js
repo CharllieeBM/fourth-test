8cd777cb1bd8a5fd293bae92cc58e768ffc79658
CharllieeBM <cburylb2@gmail.com>
2025-12-05 20:18:53 +0200

    installed react

--- src/App.js
+++ src/App.js
@@ -1,25 +1,26 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { useState, useEffect } from "react";
+import axios from "axios";
+import "./App.css";
 
-function App() {
+export default function App(props) {
+  const [temperature, setTemperature] = useState(null);
+
+  useEffect(() => {
+    const apiKey = "bc5ca568ee2d7c71357ca430a3ff8705";
+    const apiUrl = `https://api.openweathermap.org/data/2.5/weather?q=${props.city}&appid=${apiKey}&units=metric`;
+
+    axios.get(apiUrl).then((response) => {
+      setTemperature(response.data.main.temp);
+    });
+  }, [props.city]);
+
+  if (temperature === null) {
+    return <h2>Loading temperature for {props.city}...</h2>;
+  }
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <h2>
+      The temperature in {props.city} is {Math.round(temperature)}°C
+    </h2>
   );
 }
-
-export default App;
